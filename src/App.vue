5506406eed3cf3b900abde3f2d7730848140c121
Paul Krajczewski <krajczewskipawel@gmail.com>
2022-06-13 16:44:45 +0200

    todo: fix global scss

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,6 @@
 <template>
   <div id="app">
     <Navigation />
-
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/cookie-clicker">Cookie Clicker</router-link>
-    </nav>
     <router-view />
   </div>
 </template>
@@ -21,24 +16,13 @@ export default {
 </script>pt
 
 <style lang="scss">
+@import "@/assets/scss/helpers/global";
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>

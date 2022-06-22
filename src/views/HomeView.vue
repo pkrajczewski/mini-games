33b4d8efbe930480ee57305f8fa49618f9a9c07c
pkrajczewski <krajczewskipawel@gmail.com>
2022-06-22 16:00:15 +0200

    todo: fix money reset on interval

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,13 @@
-<template><div class="xd">xd</div></template>
+<template>
+  <div class="hello">
+    <h1 class="hello__title">
+      Hello there! <br />
+      This page is under construction, but You can visit Cookie Clicker game!
+      <br />
+      It is ready to play.
+    </h1>
+  </div>
+</template>
 
 <script>
 export default {
@@ -6,3 +15,20 @@ export default {
   components: {},
 };
 </script>
+
+<style lang="scss">
+.hello {
+  width: 75%;
+  margin: 0 auto;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  &__title {
+    font-size: 1.2rem;
+    font-size: clamp(1.2rem, 0.8926829268292682rem + 1.75609756097561vw, 3rem);
+    line-height: normal;
+    font-weight: 700;
+  }
+}
+</style>
